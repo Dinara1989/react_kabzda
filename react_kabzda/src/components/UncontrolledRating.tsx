5209bc40ab7590656cd5d1b603f8aa24d837d124
Dinara <dinara198989@bk.ru>
2022-06-27 14:05:05 +0100

    callback refactoring

--- react_kabzda/src/components/UncontrolledRating.tsx
+++ react_kabzda/src/components/UncontrolledRating.tsx
@@ -4,22 +4,22 @@ const UncontrolledRating = () => {
     let [value, setValue] = useState(0);
     return (
         <div>
-            <Star selected={value > 0}/><button onClick={ () => {setValue(1)}}>1</button>
-            <Star selected={value > 1}/><button onClick={ () => {setValue(2)}}>2</button>
-            <Star selected={value > 2}/><button onClick={ () => {setValue(3)}}>3</button>
-            <Star selected={value > 3}/><button onClick={ () => {setValue(4)}}>4</button>
-            <Star selected={value > 4}/><button onClick={ () => {setValue(5)}}>5</button>
+            <Star selected={value > 0} setValue={setValue}/>
+            <Star selected={value > 1} setValue={setValue}/>
+            <Star selected={value > 2} setValue={setValue}/>
+            <Star selected={value > 3} setValue={setValue}/>
+            <Star selected={value > 4} setValue={setValue}/>
         </div>
     );
 };
 type StarPropsType = {
     selected: boolean
+    setValue: (value: 0 | 1 | 2 | 3 | 4 | 5 ) => void
 }
 function Star(props: StarPropsType) {
-    if(props.selected === true) {
-        return <span><b>Star </b></span>
-    } else {
-        return <span>Star </span>
-    }
+    return <span onClick={ () => { props.setValue(5) } }>
+        {props.selected ? <b>star</b> : "star" }
+    </span>
 }
 export default UncontrolledRating;
+
